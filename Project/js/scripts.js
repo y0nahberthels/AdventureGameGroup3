f2993f13052c72c98d1cf5ff748a6cc3d5f358b6
GlennPateetOdisee <114156124+GlennPateetOdisee@users.noreply.github.com>
2023-04-17 12:15:35 +0200

    Eerste push, middagpauze

---
+++ Project/js/scripts.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// define arrays with needed variables
+const startLocations = ['Forest', 'Desert', 'Meadow', 'Swamp', 'Abandoned Ruins'];
+const urbanDestinations = ['Medieval town', 'Steampunk city', 'Elven Land' ,'Dwarven village'];
+const homeLocations = [''];
+const playerClasses = ['Mage', 'Warrior', 'Assassin', 'Paladin', 'Adventurer'];
+const mage = {
+    name: 'Mage',
+    intelligence: ''
+}
+const beasts = {};
+const npcUrban = [];
+const npcMine = [];
+const weapon = [];
